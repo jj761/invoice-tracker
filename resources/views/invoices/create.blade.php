dfbc9575300d6411c7298db006d73b1cb6f3e34f
jithin <jithinpriv@gmail.com>
2026-06-30 14:41:56 +0530

    Fix invoices create/edit view content swap

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.app')
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-4">
-        <h4>Edit Invoice {{ $invoice->invoice_number }}</h4>
-        <a href="{{ route('invoices.show', $invoice) }}" class="btn btn-outline-secondary">Back</a>
+        <h4>New Invoice</h4>
+        <a href="{{ route('invoices.index') }}" class="btn btn-outline-secondary">Back</a>
     </div>
     <div class="card">
         <div class="card-body">
-            <form method="POST" action="{{ route('invoices.update', $invoice) }}">
+            <form method="POST" action="{{ route('invoices.store') }}">
                 @csrf
-                @method('PUT')
                 @include('invoices._form')
-                <button type="submit" class="btn btn-primary">Update Invoice</button>
+                <button type="submit" class="btn btn-primary">Create Invoice</button>
             </form>
         </div>
     </div>
